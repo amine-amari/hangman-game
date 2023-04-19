7cc816ba3dd02296640194811ac3b3a0cf1988d1
Amine Amari <amari.mohamed.amine90@gmail.com>
2023-04-19 15:42:12 +0200

    add word list and random wordToGuess selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { useState } from 'react'
+import words from './WordList.json'
 import './App.css'
 
 function App() {
 
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+  console.log(wordToGuess)
   return (
     <div className="App">
       Hello
