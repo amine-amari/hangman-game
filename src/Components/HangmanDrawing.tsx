baf85e4ea046c0f6491f2d87d74c149581e4cb81
Amine Amari <amari.mohamed.amine90@gmail.com>
2023-04-19 17:11:54 +0200

    add static HangmanDrawing

--- src/Components/HangmanDrawing.tsx
+++ src/Components/HangmanDrawing.tsx
@@ -1,6 +1,45 @@
+import './HangmanDrawing.css'
+
+const HEAD = (
+    <div className='hangman-head'/>
+)
+
+const BODY = (
+    <div className='hangman-body'/>
+)
+
+const RIGHT_ARM = (
+    <div className='right-arm'/>
+)
+
+const LEFT_ARM = (
+    <div className='left-arm'/>
+)
+
+
+const RIGHT_LEG = (
+    <div className='right-leg'/>
+)
+
+const LEFT_LEG = (
+    <div className='left-leg'/>
+)
+
+
 function HangmanDrawing() {
   return (
-    <div>HangmanDrawing</div>
+    <div className=".hangman-drawing-container">
+        {HEAD}
+        {BODY}
+        {RIGHT_ARM}
+        {LEFT_ARM}
+        {RIGHT_LEG}
+        {LEFT_LEG}
+        <div className='drop-bar'/>
+        <div className='top-bar'/>
+        <div className='upright-bar'/>
+        <div className='bottom-bar'/>
+    </div>
   )
 }
 
