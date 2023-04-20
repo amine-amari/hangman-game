35df94e872a20301b5bc8166e5f0ae38eea9be32
Amine Amari <amari.mohamed.amine90@gmail.com>
2023-04-20 12:27:10 +0200

    add static Hangman Word

--- src/Components/HangmanWord.tsx
+++ src/Components/HangmanWord.tsx
@@ -1,6 +1,20 @@
+import './HangmanWord.css'
+
 function HangmanWord() {
+
+  const word = "test"
+  const guessedLetters = ["t", "e", "g"]
+
   return (
-    <div>HangmanWord</div>
+    <div className="hangman-word-container">
+      {word.split("").map((letter, index) => (
+        <span className='letter-container' key={index}>
+          <span className={`${guessedLetters.includes(letter) ? "letter-visible" : "letter-hidden"}`}>
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
   )
 }
 
