a390b5bd491c10bb6188a7eaa00726c070d74523
Amine Amari <amari.mohamed.amine90@gmail.com>
2023-04-20 15:09:40 +0200

    add static Keyboard

--- src/Components/Keyboard.tsx
+++ src/Components/Keyboard.tsx
@@ -1,6 +1,16 @@
+import './Keyboard.css'
+
+const KEYS = [
+  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"
+]
+
 function Keyboard() {
   return (
-    <div>Keyboard</div>
+    <div className="keyboard-container">
+      {KEYS.map( key => (
+        <button className='letter-btn' key={key}>{key}</button>
+      ))}
+    </div>
   )
 }
 
